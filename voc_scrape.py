e3f3519f32205350f925fa366e668af091bb2922
Karl Parkinson <karljparkinson@gmail.com>
2021-05-25 16:17:28 -0600

    Add b1617 to scrape and add note about may 2nd change

--- voc_scrape.py
+++ voc_scrape.py
@@ -55,7 +55,7 @@ def calculate_weekly_all_cases(daily_cases, start_date, end_date):
 def should_keep(row):
     if row["Date"] == "Total" or row["Date"] == "Updated" or row["Date"] == " " or row["Date"] == "":
         return False
-    elif (row["AB_B117"] == " " or row["AB_B117"] == "") and (row["AB_B1351"] == " " or row["AB_B1351"] == "") and (row["AB_P1"] == " " or row["AB_P1"] == ""):
+    elif (row["AB_B117"] == " " or row["AB_B117"] == "") and (row["AB_B1351"] == " " or row["AB_B1351"] == "") and (row["AB_P1"] == " " or row["AB_P1"] == "") and (row["AB_B1617"] == " " or row["AB_B1617"] == ""):
         return False
     else:
         return True
@@ -68,14 +68,16 @@ def daterange(date1, date2):
     for n in range(int ((date2 - date1).days)+1):
         yield date1 + timedelta(n)
 
-def fill_in_missing_dates(b117_dict, b1351_dict, p1_dict, start_date, end_date):
+def fill_in_missing_dates(b117_dict, b1351_dict, p1_dict, b1617_dict, start_date, end_date):
     most_recent_b117_cases = 0.0
     most_recent_b1351_cases = 0.0
     most_recent_p1_cases = 0.0
+    most_recent_b1617_cases = 0.0
 
     b117_dict_cum= {}
     b1351_dict_cum= {}
     p1_dict_cum= {}
+    b1617_dict_cum = {}
     all_cum = {}
 
     for dt in daterange(start_date, end_date):
@@ -88,59 +90,72 @@ def fill_in_missing_dates(b117_dict, b1351_dict, p1_dict, start_date, end_date):
         if dt.strftime("%Y-%m-%d") in p1_dict.keys():
             most_recent_p1_cases = p1_dict[dt.strftime("%Y-%m-%d")]
 
+        if dt.strftime("%Y-%m-%d") in b1617_dict.keys():
+            most_recent_b1617_cases = b1617_dict[dt.strftime("%Y-%m-%d")]
+
         b117_dict_cum[dt.strftime("%Y-%m-%d")] = most_recent_b117_cases
         b1351_dict_cum[dt.strftime("%Y-%m-%d")] = most_recent_b1351_cases
         p1_dict_cum[dt.strftime("%Y-%m-%d")] = most_recent_p1_cases
-        all_cum[dt.strftime("%Y-%m-%d")] = most_recent_p1_cases + most_recent_b117_cases + most_recent_b1351_cases
+        b1617_dict_cum[dt.strftime("%Y-%m-%d")] = most_recent_b1617_cases
+        all_cum[dt.strftime("%Y-%m-%d")] = most_recent_p1_cases + most_recent_b117_cases + most_recent_b1351_cases + most_recent_b1617_cases
 
-    return [b117_dict_cum, b1351_dict_cum, p1_dict_cum, all_cum]
+    return [b117_dict_cum, b1351_dict_cum, p1_dict_cum, b1617_dict_cum, all_cum]
 
-def caclculate_daily_voc_cases(b117_dict_cum, b1351_dict_cum, p1_dict_cum, start_date, end_date):
+def caclculate_daily_voc_cases(b117_dict_cum, b1351_dict_cum, p1_dict_cum, b1617_dict_cum, start_date, end_date):
     most_recent_b117_cases = 0.0
     most_recent_b1351_cases = 0.0
     most_recent_p1_cases = 0.0
+    most_recent_b1617_cases = 0.0
 
     b117_dict_daily = {}
     b1351_dict_daily = {}
     p1_dict_daily = {}
+    b1617_dict_daily = {}
     all_daily = {}
 
     for dt in daterange(start_date, end_date):
         b117_cum_cases_on_day = b117_dict_cum[dt.strftime("%Y-%m-%d")]
         b1351_cum_cases_on_day = b1351_dict_cum[dt.strftime("%Y-%m-%d")]
         p1_cum_cases_on_day = p1_dict_cum[dt.strftime("%Y-%m-%d")]
+        b1617_cum_cases_on_day = b1617_dict_cum[dt.strftime("%Y-%m-%d")]
 
         b117_cases_reported_on_day = b117_cum_cases_on_day - most_recent_b117_cases
         b1351_cases_reported_on_day = b1351_cum_cases_on_day - most_recent_b1351_cases
         p1_cases_reported_on_day = p1_cum_cases_on_day - most_recent_p1_cases
+        b1617_cases_reported_on_day = b1617_cum_cases_on_day - most_recent_b1617_cases
 
         b117_dict_daily[dt.strftime("%Y-%m-%d")] = b117_cases_reported_on_day
         b1351_dict_daily[dt.strftime("%Y-%m-%d")] = b1351_cases_reported_on_day
         p1_dict_daily[dt.strftime("%Y-%m-%d")] = p1_cases_reported_on_day
-        all_daily[dt.strftime("%Y-%m-%d")] = b117_cases_reported_on_day + b1351_cases_reported_on_day + p1_cases_reported_on_day
+        b1617_dict_daily[dt.strftime("%Y-%m-%d")] = b1617_cases_reported_on_day
+        all_daily[dt.strftime("%Y-%m-%d")] = b117_cases_reported_on_day + b1351_cases_reported_on_day + p1_cases_reported_on_day + b1617_cases_reported_on_day
 
         most_recent_b117_cases = b117_cum_cases_on_day
         most_recent_b1351_cases = b1351_cum_cases_on_day
         most_recent_p1_cases = p1_cum_cases_on_day
+        most_recent_b1617_cases = b1617_cum_cases_on_day
 
-    return b117_dict_daily, b1351_dict_daily, p1_dict_daily, all_daily
+    return b117_dict_daily, b1351_dict_daily, p1_dict_daily, b1617_dict_daily, all_daily
 
-def caclculate_weekly_voc_cases(b117_dict_daily, b1351_dict_daily, p1_dict_daily, start_date, end_date):
+def caclculate_weekly_voc_cases(b117_dict_daily, b1351_dict_daily, p1_dict_daily, b1617_dict_daily, start_date, end_date):
     week = 1
     current_week_days_counted = 0
 
     b117_weekly = {}
     b1351_weekly = {}
     p1_weekly = {}
+    b1617_weekly = {}
     all_weekly = {}
 
     b117_weekly_sum = 0
     b1351_weekly_sum = 0
     p1_weekly_sum = 0
+    b1617_weekly_sum = 0
     for dt in daterange(start_date, end_date):
         b117_weekly_sum += b117_dict_daily[dt.strftime("%Y-%m-%d")]
         b1351_weekly_sum += b1351_dict_daily[dt.strftime("%Y-%m-%d")]
         p1_weekly_sum += p1_dict_daily[dt.strftime("%Y-%m-%d")]
+        b1617_weekly_sum += b1617_dict_daily[dt.strftime("%Y-%m-%d")]
 
         current_week_days_counted += 1
         if (current_week_days_counted%7 == 0):
@@ -149,21 +164,24 @@ def caclculate_weekly_voc_cases(b117_dict_daily, b1351_dict_daily, p1_dict_daily
             b117_weekly[str(week)] = b117_weekly_sum
             b1351_weekly[str(week)] = b1351_weekly_sum
             p1_weekly[str(week)] = p1_weekly_sum
-            all_weekly[str(week)] = b117_weekly_sum + b1351_weekly_sum + p1_weekly_sum
+            b1617_weekly[str(week)] = b1617_weekly_sum
+            all_weekly[str(week)] = b117_weekly_sum + b1351_weekly_sum + p1_weekly_sum + b1617_weekly_sum
 
             week += 1
 
             b117_weekly_sum = 0
             b1351_weekly_sum = 0
             p1_weekly_sum = 0
+            b1617_weekly_sum = 0
 
     if (current_week_days_counted%7 != 0):
         b117_weekly[str(week)] = b117_weekly_sum
         b1351_weekly[str(week)] = b1351_weekly_sum
         p1_weekly[str(week)] = p1_weekly_sum
-        all_weekly[str(week)] = b117_weekly_sum + b1351_weekly_sum + p1_weekly_sum
+        b1617_weekly[str(week)] = b1617_weekly_sum
+        all_weekly[str(week)] = b117_weekly_sum + b1351_weekly_sum + p1_weekly_sum + b1617_weekly_sum
 
-    return b117_weekly, b1351_weekly, p1_weekly, all_weekly
+    return b117_weekly, b1351_weekly, p1_weekly, b1617_weekly_sum, all_weekly
 
 def calculate_rolling_7_day_daily_voc_average(voc_dict):
     window_size = 7
@@ -208,6 +226,8 @@ def lambda_handler(event, context):
 
     p1_dict = {}
 
+    b1617_dict = {}
+
     b117_dict["2020-12-15"] = 1.0
     b117_dict["2021-01-07"] = 3.0
     b117_dict["2021-01-12"] = 5.0
@@ -229,6 +249,9 @@ def lambda_handler(event, context):
         if (row["AB_P1"] != " " and row["AB_P1"] != ""):
             p1_dict[date.strftime("%Y-%m-%d")] = float(row["AB_P1"])
 
+        if (row["AB_B1617"] != " " and row["AB_B1617"] != ""):
+            b1617_dict[date.strftime("%Y-%m-%d")] = float(row["AB_B1617"])
+
     # For easter weekend, assumed everything non-P1 is B117. Used https://twitter.com/cspotweet
     # as source, but had to add more cases on Monday to make everything add up. These numbers are likely not
     # completely correct, but it will shake out in the 7-day avg.
@@ -237,15 +260,16 @@ def lambda_handler(event, context):
     b117_dict["2021-04-04"] = 5749
     b117_dict["2021-04-05"] = 6223
 
-    b117_dict_cum, b1351_dict_cum, p1_dict_cum, all_cum = fill_in_missing_dates(b117_dict, b1351_dict, p1_dict, datetime(2020, 12, 15), datetime.strptime(most_recent_reporting_date, "%Y-%m-%d"))
-    b117_dict_daily, b1351_dict_daily, p1_dict_daily, all_daily = caclculate_daily_voc_cases(b117_dict_cum, b1351_dict_cum, p1_dict_cum, datetime(2020, 12, 15), datetime.strptime(most_recent_reporting_date, "%Y-%m-%d"))
-    b117_weekly, b1351_weekly, p1_weekly, all_weekly = caclculate_weekly_voc_cases(b117_dict_daily, b1351_dict_daily, p1_dict_daily, datetime(2021, 1, 25), datetime.strptime(most_recent_reporting_date, "%Y-%m-%d"))
+    b117_dict_cum, b1351_dict_cum, p1_dict_cum, b1617_dict_cum, all_cum = fill_in_missing_dates(b117_dict, b1351_dict, p1_dict, b1617_dict, datetime(2020, 12, 15), datetime.strptime(most_recent_reporting_date, "%Y-%m-%d"))
+    b117_dict_daily, b1351_dict_daily, p1_dict_daily, b1617_dict_daily, all_daily = caclculate_daily_voc_cases(b117_dict_cum, b1351_dict_cum, p1_dict_cum, b1617_dict_cum, datetime(2020, 12, 15), datetime.strptime(most_recent_reporting_date, "%Y-%m-%d"))
+    b117_weekly, b1351_weekly, p1_weekly, b1617_weekly, all_weekly = caclculate_weekly_voc_cases(b117_dict_daily, b1351_dict_daily, p1_dict_daily, b1617_dict_daily, datetime(2021, 1, 25), datetime.strptime(most_recent_reporting_date, "%Y-%m-%d"))
     rolling_averages = calculate_rolling_7_day_daily_voc_average(all_daily)
 
     variants_dict = {}
     variants_dict["B117"] = {"Cumulative": b117_dict_cum, "Daily": b117_dict_daily, "Weekly": b117_weekly}
     variants_dict["B1351"] = {"Cumulative": b1351_dict_cum, "Daily": b1351_dict_daily, "Weekly": b1351_weekly}
     variants_dict["P1"] = {"Cumulative": p1_dict_cum, "Daily": p1_dict_daily, "Weekly": p1_weekly}
+    variants_dict["B1617"] = {"Cumulative": b1617_dict_cum, "Daily": b1617_dict_daily, "Weekly": b1617_weekly}
     variants_dict["All VOC"] = {"Cumulative": all_cum, "Daily": all_daily, "7 Day Daily Average": rolling_averages, "Weekly": all_weekly}
 
     all_cases_dict = {"Daily": all_daily_cases, "Weekly": all_weekly_cases}
